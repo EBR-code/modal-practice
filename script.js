3bc7681da77d2b06b05020de1696d09c25007cc3
Edson Ragas <ragasedson@gmail.com>
2021-09-19 22:20:41 +0800

    update script

--- script.js
+++ script.js
@@ -7,5 +7,14 @@ const btnsOpenmodal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenmodal);
 
 for (let i = 0; i < btnsOpenmodal.length; i++) {
-  console.log(btnsOpenmodal[i].textContent);
+  btnsOpenmodal[i].addEventListener('click', function () {
+    console.log('Button Clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+
+  btnCloseModal.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  });
 }
