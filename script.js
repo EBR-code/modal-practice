79543c1c0e0a729645640d8b2933bffa9be21345
Edson Ragas <ragasedson@gmail.com>
2021-09-20 02:53:56 +0800

    update documentation on script.js

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 'use strict';
 
+// variables for DOM
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenmodal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenmodal);
 
+// Removes hidden class in modal and overlay to show modal
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// Adds hidden class in modal and overlay to hide modal
+const hideModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+//Loops through all the query of modals, and showModal the clicked modal.
+// notice that the function is
 for (let i = 0; i < btnsOpenmodal.length; i++) {
-  btnsOpenmodal[i].addEventListener('click', function () {
-    console.log('Button Clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-
-  btnCloseModal.addEventListener('click', function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-  });
+  btnsOpenmodal[i].addEventListener('click', showModal);
 }
+// adds hidden class to the modal on close button click
+btnCloseModal.addEventListener('click', hideModal);
+// adds hidden class to the modal on overlay click
+overlay.addEventListener('click', hideModal);
+
+//listends if the escape key is keydown and if the modal doesn't have the hidden class it then
+// triggers the hide modal function
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    hideModal();
+  }
+});
